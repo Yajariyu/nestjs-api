6bb12c3890953c51d271abffda1889742f5cfed9
yajariyu <yajariyu@gmail.com>
2022-04-16 12:52:32 -0400

    add error message in signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from './../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 // import { User, BookMark } from '@prisma/client';
 
 @Injectable()
@@ -10,14 +14,27 @@ export class AuthService {
   async signup(dto: AuthDto) {
     //generate the password hash
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002')
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        else throw error;
+      }
+    }
   }
   signin() {
     return {
